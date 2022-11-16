a5057da1db888414e9d8236e769887ad37b4fde5
paulsimroth <paul.simroth@hotmail.com>
2022-11-16 12:03:59 +0100

    platforms added

--- index.js
+++ index.js
@@ -34,20 +34,34 @@ function gamePreload() {
 
 //initial setup logic
 function gameCreate() {
+    //background
+    this.add.image(400,250,"background");
+
     //Knight created, hitbox size set, knight scaled
     knight = this.physics.add.sprite(100,100,"knight");
     knight.body.setSize(400,600,10,0);
-    knight.scaleX = 0.2;
-    knight.scaleY = 0.2;
+    knight.scaleX = 0.15;
+    knight.scaleY = 0.15;
 
     //Floor created out of crates
     crates = this.physics.add.staticGroup();
+
+    //Floor
     crates.create(40,560,"crate");
     crates.create(120,560,"crate");
     crates.create(200,560,"crate");
     crates.create(280,560,"crate");
     crates.create(360,560,"crate");
     crates.create(440,560,"crate");
+    crates.create(790,560,"crate");
+
+    //Platforms
+    crates.create(200,280,"crate");
+    crates.create(300,360,"crate");
+    crates.create(490,460,"crate");
+    crates.create(570,400,"crate");
+    crates.create(710,510,"crate");
+    crates.create(790,300,"crate");
 
     //Collider set for knight and floor
     this.physics.add.collider(crates, knight);
