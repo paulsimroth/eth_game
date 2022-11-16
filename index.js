6a6ec4575a893682691c931345cfc790357ac20b
paulsimroth <paul.simroth@hotmail.com>
2022-11-16 11:26:09 +0100

    first floor crates added, collision

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//configuration of the game
+//Configuration of the game
 const config = {
-    width: 800,
-    height: 500,
+    width: 900,
+    height: 600,
     type: Phaser.AUTO,
 
     scene:{
@@ -13,28 +13,44 @@ const config = {
     physics:{
         default: "arcade",
         arcade:{
-            gravity: {y:200},
-            debug: false
+            gravity: {y:400},
+            debug: true
         }
     }
 };
 
-//loading assets
+//Loading assets
 function gamePreload() {
-    console.log("gamePreload");
+    //Images loaded
     this.load.image("knight", "assets/knight.png");
     this.load.image("background", "assets/background.png");
+    this.load.image("crate", "assets/crate.png");
 };
 
 //initial setup logic
 function gameCreate() {
-    console.log("gameCreate");
-    this.physics.add.sprite(100,100,"knight");
+    //Knight created and scaled
+    var knight = this.physics.add.sprite(100,100,"knight");
+    knight.scaleX = 0.2;
+    knight.scaleY = 0.2;
+
+    //Floor created out of crates
+    var crates = this.physics.add.staticGroup();
+    crates.create(40,560,"crate");
+    crates.create(120,560,"crate");
+    crates.create(200,560,"crate");
+    crates.create(280,560,"crate");
+    crates.create(360,560,"crate");
+    crates.create(440,560,"crate");
+
+    //Collider set for knight and floor
+    this.physics.add.collider(crates, knight);
 };
 
-//monitoring imputs and updating game
+//Monitoring imputs and updating game
 function gameUpdate() {
-    console.log("gameUpdate");
+
 };
 
+//Game
 const game = new Phaser.Game(config);
